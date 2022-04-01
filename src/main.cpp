63e11d38183871aee93affce0c0d4a830fa46989
David Fogle <david@civo.com>
2022-03-31 22:58:58 -0500

    push of all code
    Signed-off-by: David Fogle <david@civo.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,168 @@
-/*
+/*********
   Rui Santos
-  Complete project details
-   - Arduino IDE: https://RandomNerdTutorials.com/esp32-ota-over-the-air-arduino/
-   - VS Code: https://RandomNerdTutorials.com/esp32-ota-over-the-air-vs-code/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
+  Complete project details at https://RandomNerdTutorials.com/esp32-websocket-server-arduino/
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
-*/
+*********/
 
 // Import required libraries
-#include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include "SPIFFS.h"
-#include <Arduino_JSON.h>
 #include <AsyncElegantOTA.h>
 
 // Replace with your network credentials
 const char* ssid = "Misty";
 const char* password = "Phantom3351";
 
+bool ledState = 0;
+const int ledPin = 4;
+const int powerLEDPin = 2;
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
-
-// Create a WebSocket object
 AsyncWebSocket ws("/ws");
 
-// Set number of outputs
-#define NUM_OUTPUTS  4
-
-// Assign each GPIO to an output
-int outputGPIOs[NUM_OUTPUTS] = {2, 4, 12, 14};
-
-// Initialize SPIFFS
-void initSPIFFS() {
-  if (!SPIFFS.begin(true)) {
-    Serial.println("An error has occurred while mounting SPIFFS");
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
   }
-  Serial.println("SPIFFS mounted successfully");
-}
-
-// Initialize WiFi
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
+  h1 {
+    font-size: 1.8rem;
+    color: white;
   }
-  Serial.println(WiFi.localIP());
-}
-
-String getOutputStates(){
-  JSONVar myArray;
-  for (int i =0; i<NUM_OUTPUTS; i++){
-    myArray["gpios"][i]["output"] = String(outputGPIOs[i]);
-    myArray["gpios"][i]["state"] = String(digitalRead(outputGPIOs[i]));
+  h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
   }
-  String jsonString = JSON.stringify(myArray);
-  return jsonString;
-}
+  .topnav {
+    overflow: hidden;
+    background-color: #143642;
+  }
+  body {
+    margin: 0;
+  }
+  .content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .card {
+    background-color: #F8F7F9;;
+    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+    padding-top:10px;
+    padding-bottom:20px;
+  }
+  .button {
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 5px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+   }
+   /*.button:hover {background-color: #0f8b8d}*/
+   .button:active {
+     background-color: #0f8b8d;
+     box-shadow: 2 2px #CDCDCD;
+     transform: translateY(2px);
+   }
+   .state {
+     font-size: 1.5rem;
+     color:#8c8c8c;
+     font-weight: bold;
+   }
+  </style>
+<title>ESP Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="topnav">
+    <h1>ESP WebSocket Server</h1>
+  </div>
+  <div class="content">
+    <div class="card">
+      <h2>Output - GPIO 2</h2>
+      <p class="state">state: <span id="state">%STATE%</span></p>
+      <p><button id="button" class="button">Toggle</button></p>
+    </div>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onMessage(event) {
+    var state;
+    if (event.data == "1"){
+      state = "ON";
+    }
+    else{
+      state = "OFF";
+    }
+    document.getElementById('state').innerHTML = state;
+  }
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+  }
+  function initButton() {
+    document.getElementById('button').addEventListener('click', toggle);
+  }
+  function toggle(){
+    websocket.send('toggle');
+  }
+</script>
+</body>
+</html>
+)rawliteral";
 
-void notifyClients(String state) {
-  ws.textAll(state);
+void notifyClients() {
+  ws.textAll(String(ledState));
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
-    if (strcmp((char*)data, "states") == 0) {
-      notifyClients(getOutputStates());
-    }
-    else{
-      int gpio = atoi((char*)data);
-      digitalWrite(gpio, !digitalRead(gpio));
-      notifyClients(getOutputStates());
+    if (strcmp((char*)data, "toggle") == 0) {
+      ledState = !ledState;
+      notifyClients();
     }
   }
 }
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,AwsEventType type,
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
              void *arg, uint8_t *data, size_t len) {
   switch (type) {
     case WS_EVT_CONNECT:
@@ -104,36 +181,57 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,AwsEventType t
 }
 
 void initWebSocket() {
-    ws.onEvent(onEvent);
-    server.addHandler(&ws);
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (ledState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  return String();
 }
 
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
 
-  // Set GPIOs as outputs
-  for (int i =0; i<NUM_OUTPUTS; i++){
-    pinMode(outputGPIOs[i], OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(powerLEDPin, OUTPUT);
+  // digitalWrite(powerLEDPin, LOW);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
-  initSPIFFS();
-  initWiFi();
+
+  // Print ESP Local IP Address
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, LOW);
+
+  digitalWrite(powerLEDPin, HIGH); //Pull pin high to alert pi pico of ESP prog boot
+  Serial.println(WiFi.localIP());
+
   initWebSocket();
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html",false);
+    request->send_P(200, "text/html", index_html, processor);
   });
 
-  server.serveStatic("/", SPIFFS, "/");
-
-  // Start ElegantOTA
-  AsyncElegantOTA.begin(&server);
-  
   // Start server
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
   server.begin();
 }
 
 void loop() {
   ws.cleanupClients();
+  digitalWrite(ledPin, ledState);
 }
